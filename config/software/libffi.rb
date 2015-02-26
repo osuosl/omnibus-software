f196682a84841e1a19e7649b35a2fa167fa66722
Lance Albertson <lance@osuosl.org>
2015-02-26 14:58:42 -0800

    Use newer version of libffi for ppc64le

--- config/software/libffi.rb
+++ config/software/libffi.rb
@@ -15,14 +15,14 @@
 #
 
 name "libffi"
-default_version "3.0.13"
+default_version "3.2.1"
 
 dependency "libtool"
 
-source url: "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz",
-       md5: '45f3b6dbc9ee7c7dfbbbc5feba571529'
+source url: "ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz",
+       md5: '83b89587607e3eb65c70d361f13bab43'
 
-relative_path "libffi-3.0.13"
+relative_path "libffi-3.2.1"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
